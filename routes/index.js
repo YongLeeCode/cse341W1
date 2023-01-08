7d850bf1052d5c9bd20ba0afdc59555a341806f0
Yong <yonghwan311@gmail.com>
2023-01-07 17:01:57 -0700

    make a new channels

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,12 @@ routes.get('/', (req, res) => {
   res.send('Sarah Birch');
 });
 
+routes.get('/myName', (req, res) => {
+  res.send('Yonghwan Lee');
+});
+
+routes.get('/haha', (req, res) => {
+  res.send('Haha');
+});
+
 module.exports = routes;
